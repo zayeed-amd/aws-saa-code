7e4dd40cff763d7c68e054e72270db366450be0b
Your Name <you@example.com>
2026-01-09 12:12:08 -0400

    code updates

--- amazon-eventbridge/log-ec2-stop-instance.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    print('LogEC2StopInstance')
-    print('Received event:', json.dumps(event, indent=2))
-    return {
-        'statusCode': 200,
-        'body': 'Finished'
-    }
